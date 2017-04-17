07195954753b16c33a165d77d415b6c3a10cdf23
purxiz <functionofn@gmail.com>
2017-04-17 13:57:48 -0700

    removed more stuff

--- app_main.cpp
+++ app_main.cpp
@@ -35,70 +35,6 @@ void appDrawScene() {
 	glutSwapBuffers();
 }
 
-//-------------------------------------------------------
-// A function to convert window coordinates to scene
-// We use it when a mouse event is handled
-// Arguments:
-//	x, y - the coordinates to be updated
-//-------------------------------------------------------
-void windowToScene(float& x, float& y) {
-	x = (2.0f*(x / float(width))) - 1.0f;
-	y = 1.0f - (2.0f*(y / float(height)));
-}
-
-//-------------------------------------------------------
-// A function to handle window resizing
-// Called every time the window is resized
-// Arguments:
-//	b    - mouse button that was clicked, left or right
-//	s 	 - state, either mouse-up or mouse-down
-//	x, y - coordinates of the mouse when click occured
-//-------------------------------------------------------
-void appReshapeFunc(int w, int h) {
-	// Window size has changed
-	width = w;
-	height = h;
-
-	double scale, center;
-	double winXmin, winXmax, winYmin, winYmax;
-
-	// Define x-axis and y-axis range
-	const double appXmin = -1.0;
-	const double appXmax = 1.0;
-	const double appYmin = -1.0;
-	const double appYmax = 1.0;
-
-	// Define that OpenGL should use the whole window for rendering
-	glViewport(0, 0, w, h);
-
-	// Set up the projection matrix using a orthographic projection that will
-	// maintain the aspect ratio of the scene no matter the aspect ratio of
-	// the window, and also set the min/max coordinates to be the disered ones
-	w = (w == 0) ? 1 : w;
-	h = (h == 0) ? 1 : h;
-
-	if ((appXmax - appXmin) / w < (appYmax - appYmin) / h) {
-		scale = ((appYmax - appYmin) / h) / ((appXmax - appXmin) / w);
-		center = (appXmax + appXmin) / 2;
-		winXmin = center - (center - appXmin)*scale;
-		winXmax = center + (appXmax - center)*scale;
-		winYmin = appYmin;
-		winYmax = appYmax;
-	}
-	else {
-		scale = ((appXmax - appXmin) / w) / ((appYmax - appYmin) / h);
-		center = (appYmax + appYmin) / 2;
-		winYmin = center - (center - appYmin)*scale;
-		winYmax = center + (appYmax - center)*scale;
-		winXmin = appXmin;
-		winXmax = appXmax;
-	}
-
-	// Now we use glOrtho to set up our viewing frustum
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(winXmin, winXmax, winYmin, winYmax, -1, 1);
-}
 
 
 
@@ -142,14 +78,7 @@ int main(int argc, char** argv) {
 	// Set callback for drawing the scene
 	glutDisplayFunc(appDrawScene);
 
-	// Set callback for resizing th window
-	glutReshapeFunc(appReshapeFunc);
 
-	// Set callback to handle mouse clicks
-	//glutMouseFunc(appMouseFunc);
-
-	// Set callback to handle mouse dragging
-	//glutMotionFunc(appMotionFunc);
 
 	// Set callback to handle keyboad events
 	glutKeyboardFunc(appKeyboardFunc);
