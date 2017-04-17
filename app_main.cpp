38ca3a1d12376aaf5a17d05610b7b41946534d14
purxiz <functionofn@gmail.com>
2017-04-17 13:43:19 -0700

    removed some proj0 stuff

--- app_main.cpp
+++ app_main.cpp
@@ -12,40 +12,7 @@
 
 using namespace std;
 
-// Some global variables to maintain state
 
-struct Square {
-	// The coordinates of the square
-	float x;
-	float y;
-	float w;
-	float h;
-
-	// The color of the square
-	float r;
-	float g;
-	float b;
-
-	// A constructor for square
-	Square(float x, float y, float w, float h, float r, float g, float b) {
-		this->x = x;
-		this->y = y;
-		this->w = w;
-		this->h = h;
-		this->r = r;
-		this->g = g;
-		this->b = b;
-	}
-};
-
-// A "Double Ended QUEue" to store squares
-deque<Square> squares;
-
-// Variables to store current color, initialize to black
-float red = 0.0, green = 0.0, blue = 0.0;
-
-// Store the width and height of the window
-int width = 640, height = 640;
 
 //-------------------------------------------------------
 // A function to draw the scene
@@ -61,35 +28,6 @@ void appDrawScene() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	// Set the color to the current values of the global vars
-	glColor3f(red, green, blue);
-
-	// // Set square size
-
-	// Draw all the squares stored in the double-ended queue
-	for (int i = 0; i < squares.size(); i++) {
-
-		// Set the vertex color to be whatever we stored in the square
-		glColor3f(squares[i].r, squares[i].g, squares[i].b);
-		glLineWidth(1.5);
-		glBegin(GL_LINES);
-
-		// Draw the vertex in the right position
-		glVertex2f(squares[i].x, squares[i].y);
-		glVertex2f(squares[i].x + squares[i].h, squares[i].y);
-
-		glVertex2f(squares[i].x, squares[i].y);
-		glVertex2f(squares[i].x, squares[i].y - squares[i].w);
-
-		glVertex2f(squares[i].x + squares[i].h, squares[i].y);
-		glVertex2f(squares[i].x + squares[i].h, squares[i].y - squares[i].w);
-
-		glVertex2f(squares[i].x, squares[i].y - squares[i].w);
-		glVertex2f(squares[i].x + squares[i].h, squares[i].y - squares[i].w);
-
-
-		glEnd();
-	}
 
 	// We have been drawing everything to the back buffer
 	// Swap the buffers to see the result of what we drew
@@ -163,56 +101,6 @@ void appReshapeFunc(int w, int h) {
 }
 
 
-//-------------------------------------------------------
-// A function to handle mouse clicks
-// Called every time the mouse button goes up or down
-// Arguments:
-//	b    - mouse button that was clicked, left or right
-//	s 	 - state, either mouse-up or mouse-down
-//	x, y - coordinates of the mouse when click occured
-//-------------------------------------------------------
-void appMouseFunc(int b, int s, int x, int y) {
-	if(s) return;
-	// Convert from Window to Scene coordinates
-	float mx = (float)x;
-	float my = (float)y;
-	float mw = .1;
-	float mh = .1;
-
-	windowToScene(mx, my);
-
-	// Add a square with with coordinates matching the
-	// current mouse position, and the current color values
-	squares.push_front(Square(mx, my, mw, mh, red, green, blue));
-
-	// Redraw the scene by calling appDrawScene above
-	// so that the square we added above will get painted
-	glutPostRedisplay();
-	cout << "clicked, size: " << squares.size() << endl;
-}
-
-//-------------------------------------------------------
-// A function to handle mouse dragging
-// Called every time mouse moves while button held down
-// Arguments:
-//	x, y - current coordinates of the mouse
-//-------------------------------------------------------
-/*
-void appMotionFunc(int x, int y) {
-	// Convert from Window to Scene coordinates
-	float mx = (float)x;
-	float my = (float)y;
-
-	windowToScene(mx, my);
-
-	// Similar behavior to click handler. This function
-	// allows us to paint free hand with the mouse.
-	squares.push_front(Point(mx, my, red, green, blue));
-
-	// Again, we redraw the scene
-	glutPostRedisplay();
-}
-*/
 
 //-------------------------------------------------------
 // A function to handle keyboard events
@@ -224,97 +112,12 @@ void appMotionFunc(int x, int y) {
 void appKeyboardFunc(unsigned char key, int x, int y) {
 	// Define what should happen for a given key press
 	switch (key) {
-		// Space was pressed. Erase all squares
-	case ' ':
-		squares.clear();
-		break;
 
 		// Escape was pressed. Quit the program
 	case 27:
 		exit(0);
 		break;
-
-		// The "r" key was pressed. Set global color to red
-	case 'r':
-		red = 1.0;
-		green = 0.0;
-		blue = 0.0;
-		break;
-
-		// The "g" key was pressed. Set global color to green
-	case 'g':
-		red = 0.0;
-		green = 1.0;
-		blue = 0.0;
-		break;
-
-		// The "b" key was pressed. Set global color to blue
-	case 'b':
-		red = 0.0;
-		green = 0.0;
-		blue = 1.0;
-		break;
-
-		// The "k" key was pressed. Set global color to black
-	case 'k':
-		red = 0.0;
-		green = 0.0;
-		blue = 0.0;
-		break;
-
-		// The "w" key was pressed. Set global color to white
-	case '=':
-		for(int i = 0; i < squares.size(); i++){
-			squares[i].w*=1.1;
-		}
-		break;
-	case '-':
-		for(int i = 0; i < squares.size(); i++){
-			squares[i].w*=.9;
-		}
-		break;
-	case ']':
-		for(int i = 0; i < squares.size(); i++){
-			squares[i].h*=1.1;
-		}
-		break;
-	case '[':
-		for(int i = 0; i < squares.size(); i++){
-			squares[i].h*=.9;
-		}
-		break;
-	case 'w':
-		if(squares.size() > 0){
-			squares[0].y+=.1;
-		}
-		break;
-	case 's':
-		cout << squares.size() << endl;
-		cout << "0: " << squares[0].x << ", " << squares[0].y << endl;
-		cout << "1: " << squares[1].x << ", " << squares[1].y << endl;
-		if(squares.size() > 0){
-			squares[0].y-=.1;
-		}
-		cout << squares.size();
-		break;
-	case 'd':
-		if(squares.size() > 0){
-			squares[0].x+=.1;
-		}
-		break;
-	case 'a':
-		if(squares.size() > 0){
-			squares[0].x-=.1;
-		}
-		break;
 	}
-
-	for (int i = 0; i < squares.size(); i++) {
-		squares[i].r = red;
-		squares[i].g = green;
-		squares[i].b = blue;
-	}
-
 	// After all the state changes, redraw the scene
 	glutPostRedisplay();
 }
@@ -327,7 +130,7 @@ int main(int argc, char** argv) {
 
 	// Setup window position, size, and title
 	glutInitWindowPosition(20, 60);
-	glutInitWindowSize(width, height);
+	glutInitWindowSize(640, 480);
 	glutCreateWindow("CSE165 OpenGL Demo");
 
 	// Setup some OpenGL options
@@ -343,7 +146,7 @@ int main(int argc, char** argv) {
 	glutReshapeFunc(appReshapeFunc);
 
 	// Set callback to handle mouse clicks
-	glutMouseFunc(appMouseFunc);
+	//glutMouseFunc(appMouseFunc);
 
 	// Set callback to handle mouse dragging
 	//glutMotionFunc(appMotionFunc);
