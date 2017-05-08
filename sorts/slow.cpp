36a131ff425d805e144ab55087ea506df7318ca7
purxiz <functionofn@gmail.com>
2017-05-08 14:39:00 -0700

    added slow sort

---
+++ sorts/slow.cpp
@@ -0,0 +1,12 @@
+#include "../recorder.h"
+
+void slowSort(Recorder& rec, int begin, int end){
+  if(begin >= end) return;
+  int middle = (begin + end) / 2;
+  slowSort(rec, begin, middle);
+  slowSort(rec, middle + 1, end);
+  if(rec.cmp(middle,end)){
+    rec.swap(middle, end);
+  }
+  slowSort(rec, begin, end-1);
+}
